2e143a62fc5ecc46bf83512baff98e55f5426543
Kaleb Kuo <klkuo.cx@gmail.com>
2020-08-22 19:39:23 +0800

    Use :params to show details of each restaurant

--- app.js
+++ app.js
@@ -17,6 +17,11 @@ app.get('/', (req, res) => {
   res.render('index', { restaurants: restaurantList.results })
 })
 
+app.get('/restaurants/:restaurant_id', (req, res) => {
+  const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
+  res.render('show', { restaurant: restaurant })
+})
+
 // Listen to server
 app.listen(port, () => {
   console.log(`Server is listening on http://localhost:${port}`)
