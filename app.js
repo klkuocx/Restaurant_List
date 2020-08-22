c1313b02852069c7070be4dcc47b2269a5e22450
Kaleb Kuo <klkuo.cx@gmail.com>
2020-08-22 08:19:42 +0800

    Include handlebars to build layout and partial template

--- app.js
+++ app.js
@@ -2,10 +2,15 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const exphbs = require('express-handlebars')
+
+// Set view engine
+app.set('view engine', 'handlebars')
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 
 // Set the server
 app.get('/', (req, res) => {
-  res.send('Restaurant List developed by Express')
+  res.render('index')
 })
 
 // Listen to server
