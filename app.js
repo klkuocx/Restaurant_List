fb75cd8b3a3a093e65b927819cf23745303a68ac
Kaleb Kuo <klkuo.cx@gmail.com>
2020-11-05 16:26:59 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // Include packages
 const express = require('express')
+const session = require('express-session')
 const exphbs = require('express-handlebars')
 const hbshelpers = require('handlebars-helpers')
 const bodyParser = require('body-parser')
@@ -19,6 +20,11 @@ app.engine('handlebars', exphbs({ helpers: hbshelpers(), defaultLayout: 'main' }
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+app.use(session({
+  secret: 'WhySoSerious',
+  resave: false,
+  saveUninitialized: true
+}))
 
 // Set the routes
 app.use(routes)
